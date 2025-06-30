ce8130cc2b30b5c93c4d42f50c24b001bf5edd1e
Saqib Butt <mr.saqi@gmail.com>
2025-07-01 04:10:26 +0500

    module-02-updated-code

---
+++ backend-golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/backto-devops/multi-tenant-saas/backend-golang
+
+go 1.24.3
---
+++ backend-golang/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello from my first GitOps app on ArgoCD!")
+		if err != nil {
+			return
+		}
+	})
+
+	fmt.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
